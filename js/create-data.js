7715edf5897282428dfc4d2a77961ac159ceae3b
rans0000 <rans0000@gmail.com>
2016-05-05 19:43:44 +0530

    create data with forloop done

--- js/create-data.js
+++ js/create-data.js
@@ -1,6 +1,53 @@
 /*jshint browser: true*/
-/*global $: true*/
+/*global jQuery: true*/
 
-(function(window, document, undefined){
+(function(window, document, $, undefined){
     'use strict';
-})(window, document);
+    
+    //-----------------------------------------------
+    //variable declarations
+    
+    var million = 1000000;
+    var pageSize = 300;
+    //million = 15;
+    pageSize = 5;
+    var dataSet;
+    var $progressbarStrip = $('.progress-strip');
+    var $progressbarText = $('.progress-text');
+    
+    updateProgressbar(0);
+    
+    //-----------------------------------------------
+    //event listeners
+    $(document).on('click', '#btn-gen-forloop-data', function (event) {
+        event.preventDefault();
+        dataSet = generateDataWithForloop();
+        console.log(dataSet);
+        console.log(dataSet.length);
+    });
+    
+    $(document).on('click', '.btn-plus', function (event) {
+        event.preventDefault();
+        alert('just testing...');
+    });
+    
+    
+    //-----------------------------------------------
+    //generic function defenitions
+    
+    function updateProgressbar (completePercent) {
+        $progressbarStrip.css('width', completePercent + '%');
+        $progressbarText.text(completePercent + '%');
+    }
+    
+    function generateDataWithForloop () {
+        var dataArray = [];
+        updateProgressbar(0);
+        for(var i = 0; i < million; ++i){
+            dataArray.push({name: 'User-'+i});
+            updateProgressbar((i+1)/million*100);
+        }
+        return dataArray;
+    }
+    
+})(window, document, jQuery);
